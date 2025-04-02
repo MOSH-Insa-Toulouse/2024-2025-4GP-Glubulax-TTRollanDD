c87cdfe94df075b26c6096b306d6149ad56e0ffd
Yanis <sombret@insa-toulouse.fr>
2025-04-02 15:34:51 +0200

    Tout_marche_manque_le_bluetooth

--- Kicad arduino uno/partie_software/code_arduino/code_arduino.ino
+++ Kicad arduino uno/partie_software/code_arduino/code_arduino.ino
@@ -1,10 +1,5 @@
 ////////////////////////////////////////////////initialisation constante/////////////////////////////////////////////
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-#include <ClickEncoder.h>
-#include <TimerOne.h>
-#include <Wire.h>
-#include <SPI.h>
+
 //---------------------------------MCP41100 AKA R2-----------------------------------------------------//
 
 const byte csPin = 9;            // MCP42100 chip select pin
@@ -28,31 +23,33 @@ const float bendResistance = 100000.0;  // resistance at 90 deg
 
 
 
-//---------------------------------OLED-----------------------------------------------------//
+//---------------------------------OLED et encoder-----------------------------------------------------//
 
-#define nombreDePixelsEnLargeur 128  // Taille de l'écran OLED, en pixel, au niveau de sa largeur
-#define nombreDePixelsEnHauteur 64   // Taille de l'écran OLED, en pixel, au niveau de sa hauteur
-#define brocheResetOLED -1           // Reset de l'OLED partagé avec l'Arduino (d'où la valeur à -1, et non un numéro de pin)
-#define adresseI2CecranOLED 0x3C     // Adresse de "mon" écran OLED sur le bus i2c (généralement égal à 0x3C ou 0x3D)
-
-Adafruit_SSD1306 ecranOLED(nombreDePixelsEnLargeur, nombreDePixelsEnHauteur, &Wire, brocheResetOLED);
-
-//---------------------------------Encodeur-----------------------------------------------------//
-
-#define encoder0PinA 2  //CLK Output A Do not use other pin for clock as we are using interrupt
-#define encoder0PinB 4  //DT Output B
-#define Switch 5        // Switch connection if available
-volatile int encoder0Pos = 0;
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <ClickEncoder.h>
+#include <TimerOne.h>
 
 int menuitem = 1;
+int frame = 1;
 int page = 1;
 int lastMenuItem = 1;
 
 String menuItem1 = "Config_R2";
 String menuItem2 = "Flex_vs_Capteur";
+String menuItem3 = "Reset";
+
+boolean backlight = true;
+int contrast = 60;
+int volume = 50;
 
 String Config_R2[5] = { "MINIMUM", "12.5K", "25K", "37.5K", "50k" };
+int Config_R2_int[5] = { 1000, 12500, 25000, 37500, 50000 };
+float MEASURED_R[2] = { 1, 2 };  //Rcpateur en 1 et Rflex en 2
 int selectedResistance = 0;
+int selectedMeasure = 0;
 
 boolean up = false;
 boolean down = false;
@@ -60,6 +57,9 @@ boolean middle = false;
 
 ClickEncoder *encoder;
 int16_t last, value;
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
 //---------------------------------bluetooth-----------------------------------------------------//
 
@@ -97,21 +97,11 @@ void setup() {
 
 
 
-  ////////////////////////////////////////////////Encodeur/////////////////////////////////////////////
-  pinMode(encoder0PinA, INPUT);
-  digitalWrite(encoder0PinA, HIGH);  // turn on pullup resistor
-
-  pinMode(encoder0PinB, INPUT);
-  digitalWrite(encoder0PinB, HIGH);  // turn on pullup resistor
-
-  attachInterrupt(0, doEncoder, RISING);  // encoder pin on interrupt 0 - pin2
-
-
-  ////////////////////////////////////////////////OLED/////////////////////////////////////////////
+  ////////////////////////////////////////////////Encodeur et OLED/////////////////////////////////////////////
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   turnBacklightOn();
 
-  encoder = new ClickEncoder(A1, A0, A2);
+  encoder = new ClickEncoder(4, 2, 5);
   encoder->setAccelerationEnabled(false);
 
   display.begin();
@@ -122,7 +112,6 @@ void setup() {
   Timer1.attachInterrupt(timerIsr);
 
   last = encoder->getValue();
-
 
 
   ////////////////////////////////////////////////bluetooth/////////////////////////////////////////////
@@ -143,20 +132,20 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   ////////////////////////////////////////////////Capteur/////////////////////////////////////////////
-  analogRead(A1);  // valeur de la résistance du cpateur graphène après amplification
+  float Vcapteur = analogRead(A0);
+  //float Rcapteur = (1 + ( Config_R2_int[selectedResistance] / 100000 ) ) * 10000 * ( 1024 / Vcapteur ) - 11000;
+  MEASURED_R[1] = (1 + (Config_R2_int[selectedResistance] / 100000)) * 10000 * (1024 / Vcapteur) - 11000;
 
   ////////////////////////////////////////////////FLEX/////////////////////////////////////////////
   // Read the ADC, and calculate voltage and resistance from it
   int ADCflex = analogRead(flexPin);
   float Vflex = ADCflex * VCC / 1023.0;
-  float Rflex = R_DIV * (VCC / Vflex - 1.0);
+  //float Rflex = R_DIV * (VCC / Vflex - 1.0);
+  MEASURED_R[2] = R_DIV * (VCC / Vflex - 1.0);
   //Serial.println("Resistance: " + String(Rflex) + " ohms");
 
   // Use the calculated resistance to estimate the sensor's bend angle:
-  float angle = map(Rflex, flatResistance, bendResistance, 0, 90.0);  // permet de faire une régression linéaire entre le nouveau min et le nouveau max qu'on lui a fournit
-  //Serial.println("Bend: " + String(angle) + " degrees");
-  Serial.println();                                    //à changer, écrire sur le OLED
-  Serial.println(analogRead(flexPin) * 5.0 / 1023.0);  //à changer, écrire sur le OLED
+  //float angle = map(Rflex, flatResistance, bendResistance, 0, 90.0);  // permet de faire une régression linéaire entre le nouveau min et le nouveau max qu'on lui a fournit
 
   ////////////////////////////////////////////////Encodeur/////////////////////////////////////////////
   drawMenu();
@@ -175,6 +164,12 @@ void loop() {
   if (up && page == 1) {
 
     up = false;
+    if (menuitem == 2 && frame == 2) {
+      frame--;
+    }
+    if (menuitem == 3 && frame == 3) {
+      frame--;
+    }
     lastMenuItem = menuitem;
     menuitem--;
     if (menuitem == 0) {
@@ -184,19 +179,27 @@ void loop() {
     up = false;
     selectedResistance--;
     if (selectedResistance == -1) {
-      selectedResistance = 2;
+      selectedResistance = 4;
     }
   } else if (up && page == 2 && menuitem == 2) {
     up = false;
+    selectedMeasure--;
+    if (selectedMeasure == -1) {
+      selectedMeasure = 1;
+    }
   }
 
   //We have turned the Rotary Encoder Clockwise
-
   if (down && page == 1) {
-
     down = false;
+    if (menuitem == 3 && lastMenuItem == 2) {
+      frame++;
+    }
     lastMenuItem = menuitem;
     menuitem++;
+    if (menuitem == 3) {
+      menuitem--;
+    }
 
   } else if (down && page == 2 && menuitem == 1) {
     down = false;
@@ -206,67 +209,176 @@ void loop() {
     }
   } else if (down && page == 2 && menuitem == 2) {
     down = false;
+    selectedMeasure++;
+    if (selectedMeasure == 2) {
+      selectedMeasure = 0;
+    }
   }
 
   //Middle Button is Pressed
-
   if (middle) {
     middle = false;
 
-    if (page == 1 && menuitem == 6)  // Reset
+    if (page == 1 && menuitem == 3)  // Reset
     {
       resetDefaults();
     }
 
-
-    else if (page == 1 && menuitem <= 4) {
+    else if (page == 1 && menuitem <= 2) {
       page = 2;
     } else if (page == 2) {
       page = 1;
     }
   }
-
-  // set la valeur de R_2 via l'encodeur rotatoir
-  switch (abs(encoder0Pos) % 5) {
-    case 0:
-      setPotWiper(pot0, 0);  // minimum resistance
-    case 1:
-      setPotWiper(pot0, 64);  // 25% resistance
-    case 2:
-      setPotWiper(pot0, 128);  // 50% resistance
-    case 3:
-      setPotWiper(pot0, 192);  // 75% resistance
-    case 4:
-      setPotWiper(pot0, 255);  // 100% resistance
-  }
-  if (abs(encoder0Pos) <= 20) {
-    Serial.print("Me");  //à changer, écrire sur le OLED
-    for (int i = 0; i < abs(encoder0Pos); i++) {
-      Serial.print("e");  //à changer, écrire sur le OLED
-    }
-    Serial.print("rde              ");  //à changer, écrire sur le OLED
-  } else {
-    Serial.print("ME");
-    for (int i = 0; i < abs(encoder0Pos); i++) {
-      Serial.print("E");
-    }
-    Serial.print("RDE              ");
-  }
-
-  Serial.println(encoder0Pos, DEC);  //Angle = (360 / Encoder_Resolution) * encoder0Pos
-
-
-
-  delay(3000);
 }
 
+////////////////////////////////////////////////Bluetooth/////////////////////////////////////////////
+void setupBlueToothConnection()  // fonction de configuration du module bluetooth
+{
+  /*
+  Serial.begin(speed_serial); //vitesse de bluetooth
+                              //initialisation de la connexion série (avec le module bluetooth)
 
-////////////////////////////////////////////////Encodeur/////////////////////////////////////////////
-void doEncoder() {
-  if (digitalRead(encoder0PinA) == HIGH && digitalRead(encoder0PinB) == HIGH) {
-    encoder0Pos++;
-  } else if (digitalRead(encoder0PinA) == HIGH && digitalRead(encoder0PinB) == LOW) {
-    encoder0Pos--;
+  //  Serial.print("\r\n+STBD=115200\r\n"); // fixe la vitesse du bluetooth
+  Serial.print("\r\n+STBD=9600\r\n"); // fixe la vitesse du bluetooth
+  Serial.print("\r\n+STWMOD=0\r\n"); //bluetooth en mode esclave
+  Serial.print("\r\n+STNA=Arduino"); //nom de l'appareil
+  Serial.print("\r\n+STPIN=0000\r\n");//code pin "0000"
+  Serial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
+  Serial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
+  delay(2000); // This delay is required.
+  Serial.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable 
+  delay(2000); // This delay is required.
+  Serial.flush();
+*/
+}
+////////////////////////////////////////////////Encodeur et OLED/////////////////////////////////////////////
+void drawMenu() {
+
+  if (page == 1) {
+    display.setTextSize(1);
+    display.clearDisplay();
+    display.setTextColor(WHITE, BLACK);
+    display.setCursor(15, 0);
+    display.println("    MAIN MENU");
+    display.drawFastHLine(0, 10, 128, WHITE);  //BLACK
+
+    if (menuitem == 1 && frame == 1) {
+      displayMenuItem(menuItem1, 15, true);
+      displayMenuItem(menuItem2, 25, false);
+    } else if (menuitem == 2 && frame == 1) {
+      displayMenuItem(menuItem1, 15, false);
+      displayMenuItem(menuItem2, 25, true);
+    } else if (menuitem == 3 && frame == 1) {
+      displayMenuItem(menuItem1, 15, false);
+      displayMenuItem(menuItem2, 25, false);
+      displayMenuItem(menuItem3, 35, true);
+    }
+
+    else if (menuitem == 3 && frame == 2) {
+      displayMenuItem(menuItem2, 15, false);
+      displayMenuItem(menuItem3, 25, true);
+    } else if (menuitem == 2 && frame == 2) {
+      displayMenuItem(menuItem2, 15, true);
+      displayMenuItem(menuItem3, 25, false);
+    } else if (menuitem == 3 && frame == 3) {
+      displayMenuItem(menuItem3, 15, true);
+    }
+    display.display();
+  } else if (page == 2 && menuitem == 1) {
+    displayStringMenuPage(menuItem1, Config_R2[selectedResistance]);
+  } else if (page == 2 && menuitem == 2) {
+    if (selectedMeasure == 0) {
+      displayIntMenuPage(menuItem2, MEASURED_R[2], selectedMeasure);  // Rflex
+    }                                                                 // Rflex
+    else {
+      displayIntMenuPage(menuItem2, MEASURED_R[1], selectedMeasure);  // Rcapteur
+    }
   }
-  // Serial.println (encoder0Pos, DEC);  //Angle = (360 / Encoder_Resolution) * encoder0Pos
+}
+
+void resetDefaults() {
+  contrast = 60;
+  volume = 50;
+  selectedResistance = 0;
+  setContrast();
+  backlight = true;
+  turnBacklightOn();
+}
+
+void setContrast() {
+  //display.setContrast(contrast);
+  display.display();
+}
+
+void turnBacklightOn() {
+  digitalWrite(7, LOW);
+}
+
+void turnBacklightOff() {
+  digitalWrite(7, HIGH);
+}
+
+void timerIsr() {
+  encoder->service();
+}
+
+void displayIntMenuPage(String menuItem, int value, int choix) {
+  display.setTextSize(1);
+  display.clearDisplay();
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(15, 0);
+  display.println(menuItem);
+  display.drawFastHLine(0, 10, 127, WHITE);  //BLACK
+  display.setCursor(5, 15);
+  if (choix == 0) {
+    display.println("Rflex ohm");
+  } else {
+    display.println("Rcapteur ohm");
+  }
+  display.setTextSize(1);
+  display.setCursor(5, 25);
+  display.println(value);
+  display.setTextSize(1);
+  display.display();
+}
+
+void displayStringMenuPage(String menuItem, String value) {
+  display.setTextSize(1);
+  display.clearDisplay();
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(15, 0);
+  display.println(menuItem);
+  display.drawFastHLine(0, 10, 128, WHITE);  //BLACK
+  display.setCursor(5, 15);
+  display.println("Value");
+  display.setTextSize(1);
+  display.setCursor(5, 25);
+  display.println(value);
+  display.setTextSize(1);
+  display.display();
+}
+
+void displayMenuItem(String item, int position, boolean selected) {
+  if (selected) {
+    display.setTextColor(BLACK, WHITE);  //, BLACK
+  } else {
+    display.setTextColor(WHITE);  //BLACK,
+  }
+  display.setCursor(0, position);
+  display.print(">" + item);
+}
+
+void readRotaryEncoder() {
+  value += encoder->getValue();
+
+  if (value / 2 > last) {
+    last = value / 2;
+    down = true;
+    delay(150);
+  } else if (value / 2 < last) {
+    last = value / 2;
+    up = true;
+    delay(150);
+  }
 }
